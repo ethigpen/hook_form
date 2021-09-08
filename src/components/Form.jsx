f852a324b9b6da2902d9092edd15c07381b88050
Emile Thigpen <85521988+ethigpen@users.noreply.github.com>
2021-09-08 13:20:17 -0500

    updated some stuff

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,14 +3,10 @@ import React, {useState} from 'react';
 
 const Form = () => {
     const [fname, setFirstName] = useState("");
-    // const [nameError, setNameError] = useState("");
     const [lname, setLastName] = useState("");
     const [email, setEmail] = useState("");
-    // const [emailError, setEmailError] = useState("");
     const [password, setPassword] = useState("");
-    // const [passwordError, setPasswordError] = useState("");
     const [confirmPassword, setCPassword] = useState("");
-    // const [passwordCError, setPasswordCError] = useState("");
     const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false);
 
     const createUser = (e) => {
@@ -27,63 +23,6 @@ const Form = () => {
         return "Welcome, please submit the form";
 	}
     };
-
-    // const handleFName = (e) => {
-    //     setFirstName(e.target.value);
-    //     if(e.target.value.length < 1) {
-    //         setNameError("First name is required!");
-    //     } else if(e.target.value.length < 2) {
-    //         setNameError("First name must be 2 characters or longer!");
-    //     }else{
-    //         setNameError("");
-    //     }
-    // };
-
-    // const handleLName = (e) => {
-    //     setLastName(e.target.value);
-    //     if(e.target.value.length < 1) {
-    //         setNameError("Last name is required!");
-    //     } else if(e.target.value.length < 2) {
-    //         setNameError("Last name must be 2 characters or longer!");
-    //     }else{
-    //         setNameError("");
-    //     }
-    // };
-
-    // const handleEmail = (e) => {
-    //     setEmail(e.target.value);
-    //     if(e.target.value.length < 1) {
-    //         setEmailError("Email is required!");
-    //     } else if(e.target.value.length < 5) {
-    //         setEmailError("Email must be 5 characters or longer!");
-    //     }else{
-    //         setEmailError("");
-    //     }
-    // };
-
-    // const handlePassword = (e) => {
-    //     setPassword(e.target.value);
-    //     if(e.target.value.length < 1) {
-    //         setPasswordError("Password is required!");
-    //     } else if(e.target.value.length < 8) {
-    //         setPasswordError("Password must be 8 characters or longer!");
-    //     } else{
-    //         setPasswordError("");
-    //     }
-    // };
-
-    // const handleCPassword = (e) => {
-    //     setCPassword(e.target.value);
-    //     if(e.target.value.length < 1) {
-    //         setPasswordCError("Password is required!");
-    //     } else if(e.target.value.length < 8) {
-    //         setPasswordCError("Password must be 8 characters or longer!");
-    //     } else if(password !== confirmPassword){
-    //         setPasswordCError("Passwords must match!");
-    //     }else if (password == confirmPassword){
-    //         setPasswordCError("");
-    //     }
-    // };
 
     return (
         <div className="container">
